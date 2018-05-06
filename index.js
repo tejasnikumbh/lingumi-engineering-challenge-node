f28e2fb16bafd5249e1f72fe256a7de8c3bb0c18
Tejas Nikumbh <tejasnikumbh@gmail.com>
2018-05-06 08:15:28 +0530

    Draft for solution(1.0 - Untested and non-functional)

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
 const _ = require('lodash');
+
+/**
+* Module for solution to engineering-challenge-node for lingumi-software
+*
+* The general idea for the function is to compute scores and associate them
+* with given words. We then sort the words by the score and return the first
+* three words.
+*
+* Author:- Tejas Nikumbh
+* Copyright:- Tejas Nikumbh, 2018
+**/
+
+// Core interface function that selects 3 words (or throws error)
 const getWordIds = (words, idsLearnedInLesson) => {
   // Perform input validation
-  // Filter out words that do not meet criteria
-  // return selectWords(validWords)
+  if(!words || words.length < 3) { throw new Error('invalid input - words')}
+  var validWords = words.filter((word) => word.numberOfTimesLearned > 0);
+  if(validWords.length < 3) { throw new Error('invalid input - less than 3 valid words')}
+  // Return selected words
+  return _selectWords(validWords, idsLearnedInLesson);
 }
 
-const selectWords = (validWords, idsLearnedInLesson) => {
-  var scores = computeScores(validWords, idsLearnedInLesson);
+// Selects  3 words from valid words by sorting them according to score
+const _selectWords = (validWords, idsLearnedInLesson) => {
+  var scores = _computeScores(validWords, idsLearnedInLesson);
+
   var wordsAndScores = _.zip(validWords, scores);
   var wordsSortedByScores = _.sortBy(wordsAndScores, (tuple) => {
     return tuple[1]; // tuple[1] is score
   });
+
   var selectedTuples = wordsSortedByScores.slice(0,3);
   var selectedWords = tuples.map((tuple) => tuple[0]);
+
   return selectedWords;
 }
 
-const computeScores = (word, idsLearnedInLesson) => {
+// Computes priority score for a given word
+const _computeScores = (word, idsLearnedInLesson) => {
   // Params represent weights as follows
   // params[0] -> weight for hasAlreadyCollected
   // params[1] -> weight for numberOfTimesLearned
